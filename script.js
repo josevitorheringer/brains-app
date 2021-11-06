3840a6126451d2fb36a34dc9034fbc0a5c0abcb1
José Vítor Heringer <46639621+josevitorheringer@users.noreply.github.com>
2021-11-06 19:01:41 -0300

    blink time adjusted

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ let shuffleOrder = () => {
   let colorOrder = Math.floor(Math.random() * 4);
   order[order.length] = colorOrder;
   clickedOrder = [];
-
   for (let i in order) {
     let elementColor = createColorElement(order[i]);
     lightColor(elementColor, Number(i) + 1);
@@ -77,7 +76,9 @@ let createColorElement = (color) => {
 
 // Next level function
 let nextLevel = () => {
-  shuffleOrder();
+  setTimeout(() => {
+    shuffleOrder();
+  }, 500);
 };
 
 // Game Over function
